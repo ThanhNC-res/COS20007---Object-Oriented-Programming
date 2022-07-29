2d623ddd99382c2664507155a6fcf171c41e5947
ThanhNC-res <110225420+ThanhNC-res@users.noreply.github.com>
2022-07-29 17:27:29 +0700

    Add files via upload

---
+++ Task 3.2P/Clock/Counter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clock
+{
+    public class Counter
+    {
+        private string _part;
+        private int _count;
+
+        public Counter(string part, int count)
+        {
+            _part = part;
+            _count = count;
+        }
+
+        public string Part { get => _part; set => _part = value; }
+        public int Count { get => _count; set => _count = value; }
+
+        public int CountIncre()
+        {
+            return _count += 1;
+        }
+
+        public int ResetCount()
+        {
+            return _count = 0;
+        }
+
+    }
+}
---
+++ Task 3.2P/Clock/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Clock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Clock myClock = new Clock();
+            while (true)
+            {
+                myClock.Tick();
+                Console.Clear();
+                Console.WriteLine(myClock.ClockFormat());
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
